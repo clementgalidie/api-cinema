65cd83f1f78712438165c10ebedea276512ebfa7
Clément Galidie <clement-galidie@cifacom.com>
2014-07-09 01:34:13 +0200

    Add function to load class, spl __autoload stack and routes.

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 	header('content-type: application/json; charset=utf-8'); // Set utf-8.
 	require 'toro.php';
 	require 'includes/database.php';
-	require 'handlers/users.php';
+
+	// Function to load each class.
+	function loadClass($class) {
+		require 'handlers/' . $class . '.class.php';
+	}
+
+	// Register loadClass function with the spl provided __autoload stack.
+	spl_autoload_register('loadClass');
 
 	// Defined routes.
 	Toro::serve(array(
-		'/v1/users/' => 'users'
+		'/v1/users/'                   => 'users',
+		'/v1/users/:number/'           => 'userById',
+		'/v1/users/deleting/:number/'  => 'deleteUserById',
+		'/v1/movies/'                  => 'movies',
+		'/v1/movies/:number/'          => 'moviesById',
+		'/v1/movies/deleting/:number/' => 'deleteMoviesById'
 		));
 ?>
