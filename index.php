3445696537c2bd3c34f60f0abd855056f40069c8
Clément Galidie <clement-galidie@cifacom.com>
2014-07-09 16:42:20 +0200

    Change routes and add errors messages.

--- index.php
+++ index.php
@@ -11,13 +11,30 @@
 	// Register loadClass function with the spl provided __autoload stack.
 	spl_autoload_register('loadClass');
 
+	// Defined HTTP errors.
+	ToroHook::add('400', function() {
+		echo 'Bad Request';
+	});
+	ToroHook::add('401', function() {
+		echo 'Unauthorized';
+	});
+	ToroHook::add('403', function() {
+		echo 'Forbidden';
+	});
+	ToroHook::add('404', function() {
+		echo 'Not Found.';
+	});
+
+	// Defined server error.
+	ToroHook::add('500', function() {
+		echo '500 Internal Server Error';
+	});
+
 	// Defined routes.
 	Toro::serve(array(
-		'/v1/users/'                   => 'users',
-		'/v1/users/:number/'           => 'userById',
-		'/v1/users/deleting/:number/'  => 'deleteUserById',
-		'/v1/movies/'                  => 'movies',
-		'/v1/movies/:number/'          => 'moviesById',
-		'/v1/movies/deleting/:number/' => 'deleteMoviesById'
+		'/v1/users/'          => 'users',
+		'/v1/users/:number/'  => 'userId',
+		'/v1/movies/'         => 'movies',
+		'/v1/movies/:number/' => 'movieId'
 		));
 ?>
