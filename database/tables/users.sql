7d39dd74e7dc7d0f762cf943aaccbcb578af0c33
Clément Galidie <clement-galidie@cifacom.com>
2014-07-09 16:58:13 +0200

    Add tables SQL files.

---
+++ database/tables/users.sql
@@ -0,0 +1,15 @@
+--
+-- Database: `cinema-api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- `users` table structure
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
